667d4973978067f4d71ef1f1b0af2cdd87ab7382
asahiko123 <66983728+asahiko123@users.noreply.github.com>
2023-03-05 21:53:07 +0900

    make migration -Item Table

--- database/migrations/2023_03_05_123626_create_items_table.php
+++ database/migrations/2023_03_05_123626_create_items_table.php
@@ -15,6 +15,9 @@ return new class extends Migration
     {
         Schema::create('items', function (Blueprint $table) {
             $table->id();
+            $table->integer('price');
+            $table->boolean('is_selling')->default(true);
+            $table->string('memo')->nullable();
             $table->timestamps();
         });
     }
